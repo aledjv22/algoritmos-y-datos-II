f82ee0d90e672dc84c56f4627921611a9e89353a
Alejandro Díaz <victor.diaz.jauregui@mi.unc.edu.ar>
2024-03-17 02:24:32 -0300

    Ejercicio 01 completado

--- Laboratorio_01/ej1/main.c
+++ Laboratorio_01/ej1/main.c
@@ -7,6 +7,14 @@
 /* Maximum allowed length of the array */
 #define MAX_SIZE 100000
 
+/* Read status */
+// fscanf returns 1 when it reads a number
+#define READ_SUCCESS 1 
+// fscanf returns 0 when it can't read a number
+#define READ_FAILURE 0
+// fscanf returns EOF when it reaches the end of the file
+#define READ_EOF EOF 
+
 void print_help(char *program_name) {
     /* Print the usage help of this program. */
     printf("Usage: %s <input file path>\n\n"
@@ -41,14 +49,71 @@ char *parse_filepath(int argc, char *argv[]) {
     return result;
 }
 
+// Read the array from a file
 unsigned int array_from_file(int array[],
                              unsigned int max_size,
                              const char *filepath) {
-    //your code here!!!
+    unsigned int length; 
+    // Open the file in read mode
+    FILE *file = fopen(filepath, "r");
+    // Check if the file was opened successfully
+    if(file == NULL) {
+        printf("Error, the file %s could not be opened.\n", filepath);
+        exit(EXIT_FAILURE);
+    }
+
+    // Read the length of the array
+    fscanf(file, "%u", &length);
+
+    // Check if the length is greater than the maximum allowed
+    if(length > max_size) {
+        printf("Error, the read size is greater than the allowed size.\n");
+        fclose(file);
+        exit(EXIT_FAILURE);
+    }
+
+    unsigned int i;
+    for(i= 0; i < length; ++i) {
+        // Read each number and store it in the array
+        int read_status = fscanf(file, "%d", &array[i]);
+
+        // Check if the read was successful
+        if(read_status == READ_FAILURE) {
+            printf("Error, the reading could not be performed.\n");
+            fclose(file);
+            exit(EXIT_FAILURE);
+        }
+
+        // Check if the file ended before reading all the numbers
+        if(read_status == READ_EOF) {
+            printf("Error, the indicated size is greater than the number of numbers.\n");
+            fclose(file);
+            exit(EXIT_FAILURE);
+        }
+    }
+    
+    // Check if the file has more numbers than the length indicates
+    if(fscanf(file, "%d", &array[length]) == READ_SUCCESS) {
+        printf("Error, the indicated size is less than the number of numbers.\n");
+        fclose(file);
+        exit(EXIT_FAILURE);
+    }
+
+    // Close the file
+    fclose(file);
+
+    return length;
 }
 
+// Print the array
 void array_dump(int a[], unsigned int length) {
-    //your code here!!!
+    printf("\nYour array is: [");
+
+    for(unsigned int i = 0; i < length; ++i) {
+        printf("%d", a[i]);
+        if(i < length - 1) printf (", ");
+    }
+    printf("]\n");
 }
 
 
