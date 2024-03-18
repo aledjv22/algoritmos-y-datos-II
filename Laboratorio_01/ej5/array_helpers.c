d5114f8e75ed3507e93aacd892fa9822f7f1451d
Alejandro Díaz <victor.diaz.jauregui@mi.unc.edu.ar>
2024-03-17 22:30:33 -0300

    Ejercicio 05 completado

---
+++ Laboratorio_01/ej5/array_helpers.c
@@ -0,0 +1,85 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "array_helpers.h"
+#include "mybool.h"
+
+// Read the array from a file
+unsigned int array_from_file(int array[],
+                             unsigned int max_size,
+                             const char *filepath) {
+    unsigned int length; 
+    // Open the file in read mode
+    FILE *file = fopen(filepath, "r");
+    // Check if the file was opened successfully
+    if(file == NULL) {
+        printf("Error, the file %s could not be opened.\n", filepath);
+        exit(EXIT_FAILURE);
+    }
+
+    // Read the length of the array
+    fscanf(file, "%u", &length);
+
+    // Check if the length is greater than the maximum allowed
+    if(length > max_size) {
+        printf("Error, the read size is greater than the allowed size.\n");
+        fclose(file);
+        exit(EXIT_FAILURE);
+    }
+
+    unsigned int i;
+    for(i= 0; i < length; ++i) {
+        // Read each number and store it in the array
+        int read_status = fscanf(file, "%d", &array[i]);
+
+        // Check if the read was successful
+        if(read_status == READ_FAILURE) {
+            printf("Error, the reading could not be performed.\n");
+            fclose(file);
+            exit(EXIT_FAILURE);
+        }
+
+        // Check if the file ended before reading all the numbers
+        if(read_status == READ_EOF) {
+            printf("Error, the indicated size is greater than the number of numbers.\n");
+            fclose(file);
+            exit(EXIT_FAILURE);
+        }
+    }
+    
+    // Check if the file has more numbers than the length indicates
+    if(fscanf(file, "%d", &array[length]) == READ_SUCCESS) {
+        printf("Error, the indicated size is less than the number of numbers.\n");
+        fclose(file);
+        exit(EXIT_FAILURE);
+    }
+
+    // Close the file
+    fclose(file);
+
+    return length;
+}
+
+// Print the array
+void array_dump(int a[], unsigned int length) {
+    printf("\nYour array is: [");
+
+    for(unsigned int i = 0; i < length; ++i) {
+        printf("%d", a[i]);
+        if(i < length - 1) printf (", ");
+    }
+    printf("]\n");
+}
+
+// Check if the array is sorted
+mybool array_is_sorted(int a[], unsigned int length) {
+    mybool is_sorted = true;
+    unsigned int index = 0;
+
+    while(is_sorted && (index < length - 1)) {
+        is_sorted = is_sorted && (a[index] <= a[index + 1]);
+        index++;
+    }
+    
+    return is_sorted;
+}
--- Laboratorio_01/ej5/array_helpers.h
+++ Laboratorio_01/ej5/array_helpers.h
@@ -1,9 +1,25 @@
-#include <stdbool.h>
+#ifndef _ARRAY_HELPERS_H_
+#define _ARRAY_HELPERS_H_
 
-unsigned int array_from_file(int array[],
-                             unsigned int max_size,
-                             const char *filepath); 
+#include "mybool.h"
 
+/* Maximum allowed length of the array */
+#define MAX_SIZE 100000
+
+/* Read status */
+// fscanf returns 1 when it reads a number
+#define READ_SUCCESS 1 
+// fscanf returns 0 when it can't read a number
+#define READ_FAILURE 0
+// fscanf returns EOF when it reaches the end of the file
+#define READ_EOF EOF 
+
+// Read the array from a file
+unsigned int array_from_file(int array[], unsigned int max_size, const char *filepath);
+
+// Print the array
 void array_dump(int a[], unsigned int length);
 
-bool array_is_sorted(int a[], unsigned int length);
+mybool array_is_sorted(int a[], unsigned int length);
+
+#endif
