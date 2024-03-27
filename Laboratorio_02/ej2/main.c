952a74f3de99c8ac31b66641015c0061702d00dc
Alejandro Díaz <victor.diaz.jauregui@mi.unc.edu.ar>
2024-03-26 23:38:46 -0300

    Base de la resolucion ej02-lab02

--- Laboratorio_02/ej2/main.c
+++ Laboratorio_02/ej2/main.c
@@ -58,6 +58,9 @@ int main(int argc, char *argv[]) {
     int copy[MAX_SIZE];
     array_copy(copy, array, length);
 
+    /* Implementation of the 'quick_sort' function */
+    quick_sort(array, length);
+
     /* show the ordered array in the screen */
     array_dump(array, length);
 
--- Laboratorio_02/ej2/sort.c
+++ Laboratorio_02/ej2/sort.c
@@ -8,12 +8,11 @@
 
 
 static void quick_sort_rec(int a[], unsigned int izq, unsigned int der) {
- /* needs implementation */
+    unsigned int pivot = partition(a, izq, der);
 
-    /* no implementes partition, ya está implementado en sort_helpers.o
-       (no se puede leer, pero en sort_helpers.h vas a encontrar información
-        para saber cómo usarlo)
-    */
+    if (izq < pivot) quick_sort_rec(a, izq, pivot-1);
+
+    if (pivot < der) quick_sort_rec(a, pivot+1, der);
 }
 
 void quick_sort(int a[], unsigned int length) {
