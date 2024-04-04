6b9864cc2ca5ff28fecfa5d077631a0779c12e23
Alejandro Díaz <victor.diaz.jauregui@mi.unc.edu.ar>
2024-04-03 23:53:14 -0300

    Copiado de las funciones al sort.c

--- Laboratorio_02/ej4/sort.c
+++ Laboratorio_02/ej4/sort.c
@@ -26,7 +26,11 @@ void selection_sort(int a[], unsigned int length) {
 
 
 static void insert(int a[], unsigned int i) {
-    /* copiá acá la implementación que hiciste en el ejercicio 1 */
+    unsigned int j = i;
+    while (j > 0 && goes_before(a[j], a[j-1])){
+        swap(a, j-1, j);
+        --j;
+    }
 }
 
 void insertion_sort(int a[], unsigned int length) {
@@ -37,11 +41,34 @@ void insertion_sort(int a[], unsigned int length) {
 
 
 static unsigned int partition(int a[], unsigned int izq, unsigned int der) {
-    /* copiá acá la implementación que hiciste en el ejercicio 3 */
+    unsigned int pivot = izq;
+    unsigned int i = izq + 1;
+    unsigned int j = der;
+
+    while (i <= j) {
+        if (goes_before(a[i], a[pivot])) {
+            ++i;
+        } else if (goes_before(a[pivot], a[j])) {
+            --j;
+        } else {
+            swap(a, i, j);
+            ++i;
+            --j;
+        }
+    }
+
+    swap(a, pivot, j);
+    pivot = j;
+
+    return pivot;
 }
 
 static void quick_sort_rec(int a[], unsigned int izq, unsigned int der) {
-    /* copiá acá la implementación que hiciste en el ejercicio 2 */
+    unsigned int pivot = partition(a, izq, der);
+
+    if (izq < pivot) quick_sort_rec(a, izq, pivot-1);
+
+    if (pivot < der) quick_sort_rec(a, pivot+1, der);
 }
 
 void quick_sort(int a[], unsigned int length) {
