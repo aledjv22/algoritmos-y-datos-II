9c66b3663ec5fb6c08394d679a35b8c6d39fd836
Alejandro Díaz <victor.diaz.jauregui@mi.unc.edu.ar>
2024-04-04 00:48:00 -0300

    Funciones del fixstring.c completadas

--- Laboratorio_02/ej5/fixstring.c
+++ Laboratorio_02/ej5/fixstring.c
@@ -5,15 +5,33 @@
 
 
 unsigned int fstring_length(fixstring s) {
-    /* copiá acá la implementación que hiciste en el ejercicio 0 */
+    unsigned int length = 0;
+
+    while (s[length] != '\0') length++;
+
+    return length;
 }
 
 bool fstring_eq(fixstring s1, fixstring s2) {
-    /* copiá acá la implementación que hiciste en el ejercicio 0 */
+    bool eq = true;
+    unsigned int i = 0;
+
+    while (s1[i] != '\0' && s2[i] != '\0' && eq) {
+        eq = s1[i] == s2[i];
+        i++;
+    }
+
+    eq = eq && s1[i] == '\0' && s2[i] == '\0';
+
+    return eq;
 }
 
 bool fstring_less_eq(fixstring s1, fixstring s2) {
-    /* copiá acá la implementación que hiciste en el ejercicio 0 */
+    unsigned int i = 0;
+
+    while (s1[i] != '\0' && s2[i] != '\0' && s1[i] == s2[i]) i++;
+
+    return s1[i] <= s2[i];
 }
 
 void fstring_set(fixstring s1, const fixstring s2) {
@@ -27,10 +45,9 @@ void fstring_set(fixstring s1, const fixstring s2) {
 
 void fstring_swap(fixstring s1,  fixstring s2) {
     fixstring aux;
-    /*
-     * COMPLETAR
-     *
-     */
+    fstring_set(aux, s1);
+    fstring_set(s1, s2);
+    fstring_set(s2, aux);
 }
 
 
