79542156ae592fd20677b4d703dc4c1e3c1c8827
Alejandro Díaz <victor.diaz.jauregui@mi.unc.edu.ar>
2024-04-04 00:53:06 -0300

    Copiadas las funciones

--- Laboratorio_02/ej5/sort.c
+++ Laboratorio_02/ej5/sort.c
@@ -10,11 +10,34 @@
 
 
 static unsigned int partition(fixstring a[], unsigned int izq, unsigned int der) {
-    /* copiá acá la implementación que hiciste en el ejercicio 3 */
+    unsigned int pivot = izq;
+    unsigned int i = izq + 1;
+    unsigned int j = der;
+
+    while (i <= j) {
+        if (goes_before(a[i], a[pivot])) {
+            ++i;
+        } else if (goes_before(a[pivot], a[j])) {
+            --j;
+        } else {
+            swap(a, i, j);
+            ++i;
+            --j;
+        }
+    }
+
+    swap(a, pivot, j);
+    pivot = j;
+
+    return pivot;
 }
 
 static void quick_sort_rec(fixstring a[], unsigned int izq, unsigned int der) {
-    /* copiá acá la implementación que hiciste en el ejercicio 3 */
+    unsigned int pivot = partition(a, izq, der);
+
+    if (izq < pivot) quick_sort_rec(a, izq, pivot-1);
+
+    if (pivot < der) quick_sort_rec(a, pivot+1, der);
 }
 
 void quick_sort(fixstring a[], unsigned int length) {
