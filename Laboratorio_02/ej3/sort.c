4ffdbc34587893c8e6045224525e488c9cf73fbc
Alejandro Díaz <victor.diaz.jauregui@mi.unc.edu.ar>
2024-04-03 22:25:52 -0300

    Ej03-Lab02 completado

--- Laboratorio_02/ej3/sort.c
+++ Laboratorio_02/ej3/sort.c
@@ -9,13 +9,31 @@
 
 
 static unsigned int partition(int a[], unsigned int izq, unsigned int der) {
-    /* Needs implementation */
-
     /* Permutes elements of a[izq..der] and returns pivot such that:
      - izq <= pivot <= der
      - elements in a[izq,pivot) all 'go_before' (according to function goes_before) a[pivot]
      - a[pivot] 'goes_before' all the elements in a(pivot,der]
     */
+    unsigned int pivot = izq;
+    unsigned int i = izq + 1;
+    unsigned int j = der;
+
+    while (i <= j) {
+        if (goes_before(a[i], a[pivot])) {
+            ++i;
+        } else if (goes_before(a[pivot], a[j])) {
+            --j;
+        } else {
+            swap(a, i, j);
+            ++i;
+            --j;
+        }
+    }
+
+    swap(a, pivot, j);
+    pivot = j;
+
+    return pivot;
 }
 
 static void quick_sort_rec(int a[], unsigned int izq, unsigned int der) {
