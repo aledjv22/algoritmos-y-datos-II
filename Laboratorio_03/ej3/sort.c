678d5ee582ad848988fbc9ec7d8fc39fd1a96782
Victor Alejandro Díaz Jáuregui <victor.diaz.jauregui@mi.unc.edu.ar>
2024-04-18 13:58:30 -0300

    Ej03-Lab03 completado

--- Laboratorio_03/ej3/sort.c
+++ Laboratorio_03/ej3/sort.c
@@ -12,8 +12,7 @@
 #include "player.h"
 
 bool goes_before(player_t x, player_t y){
-    // completar aquí
-    return true;
+    return (x.rank <= y.rank);
 }
 
 bool array_is_sorted(player_t atp[], unsigned int length) {
@@ -24,7 +23,41 @@ bool array_is_sorted(player_t atp[], unsigned int length) {
     return (i == length);
 }
 
-void sort(player_t a[], unsigned int length) {
-    // completar aquí
+static void swap(player_t a[], unsigned int i, unsigned int j) {
+    player_t aux;
+    aux = a[i];
+    a[i] = a[j];
+    a[j] = aux;
 }
 
+static unsigned int partition(player_t a[], unsigned int izq, unsigned int der) {
+    unsigned int i = izq + 1, j = der, pivot = izq;
+    while (i <= j) {
+        if (goes_before(a[i], a[pivot])){
+            ++i;
+        }else if (goes_before(a[pivot], a[j])){
+            --j;
+        }else if(goes_before(a[pivot], a[i]) && goes_before(a[j], a[pivot])){
+            swap(a, i, j);
+            ++i;
+            --j;
+        }        
+    }
+    swap(a, pivot, j);
+    pivot = j;
+    return pivot;
+}
+
+static void quick_sort_rec(player_t a[], unsigned int izq, unsigned int der) {
+    unsigned int pivot = partition(a, izq, der);
+    if (izq < pivot) {
+        quick_sort_rec(a, izq, pivot - 1u);
+    }
+    if (pivot < der){
+        quick_sort_rec(a, pivot + 1, der);
+    }
+}
+
+void sort(player_t a[], unsigned int length) {
+    quick_sort_rec(a, 0u, (length == 0u) ? 0u : length - 1u);
+}
