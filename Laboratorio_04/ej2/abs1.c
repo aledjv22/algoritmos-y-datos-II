30c651c773dac7f4c1bd0c86e6de9c6637d01309
Victor Alejandro Díaz Jáuregui <victor.diaz.jauregui@mi.unc.edu.ar>
2024-06-11 19:54:38 -0300

    Ej02A-Lab04 finalizado

--- Laboratorio_04/ej2/abs1.c
+++ Laboratorio_04/ej2/abs1.c
@@ -1,17 +1,30 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-void absolute(int x, int y) {
-    //
-    // Completar aquí
-    //
+void absolute(int x, int y)
+{
+    if (x >= 0)
+    {
+        y = x;
+    }
+    else
+    {
+        y = -x;
+    }
 }
 
-int main(void) {
+int main(void)
+{
     int a=0, res=0;
-    //
-    // Completar aquí
-    //
+    a = -10;
+    absolute(a, res);
+    printf("El valor absoluto de %d es %d\n", a, res);
+    res = 0;
+
     return EXIT_SUCCESS;
 }
 
+/*
+    El valor que se muestra por pantalla es 0.
+    El programa no coincide con el del lenguaje teórico.
+*/
