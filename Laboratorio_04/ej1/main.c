be56e05b34749b7c7682759e0c6bc899590cf7b2
Victor Alejandro Díaz Jáuregui <victor.diaz.jauregui@mi.unc.edu.ar>
2024-06-11 18:59:13 -0300

    Ej01-Lab04 finalizado

--- Laboratorio_04/ej1/main.c
+++ Laboratorio_04/ej1/main.c
@@ -6,7 +6,8 @@
 #include <stdlib.h>
 
 /** @brief structure which represent a person */
-typedef struct _person {
+typedef struct _person
+{
     int age;
     char name_initial;
 } person_t;
@@ -16,25 +17,27 @@ typedef struct _person {
  *
  * @return EXIT_SUCCESS when programs executes correctly, EXIT_FAILURE otherwise
  */
-int main(void) {
-
+int main(void)
+{
+    // Declaración de punteros
+    int *number = NULL;
+    char *letter = NULL;
+    // Declaración e inicializado de variables a modificar
     int x = 1;
     person_t m = {90, 'M'};
     int a[] = {0, 1, 2, 3};
-
-    /* Completar aquí:
-       Al finalizar la ejecución las variables deben tener los siguientes valores
-       x = 9
-       m = (100, F)
-       a[1] = 42
-
-       Las restricciones son:
-       - No usar las variables 'x', 'm' y 'a' en la parte izquierda de alguna asignación.
-       - Se pueden agregar líneas de código, pero no modificar las que ya existen.
-       - Se pueden declarar hasta 2 punteros.
-       AYUDA: podes usar GDB para consultares valores de variables y la dirección de memoria donde estan alojadas.
-    */
-
+    // Modificación del valor en x
+    number = &x;
+    *number = 9;
+    // Modificación del valor de m
+    number = &m.age;
+    *number = 100;
+    letter = &m.name_initial;
+    *letter = 'F';
+    // Modificación del valor de a[1]
+    number = &a[1];
+    *number = 42;
+    // Impreso de los resultados.
     printf("x = %d\n", x);
     printf("m = (%d, %c)\n", m.age, m.name_initial);
     printf("a[1] = %d\n", a[1]);
