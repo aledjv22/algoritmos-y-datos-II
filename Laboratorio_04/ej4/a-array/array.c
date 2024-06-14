6ed96dcddeb4586be541b8e6d700b3bcc19de9c7
Victor Alejandro Díaz Jáuregui <victor.diaz.jauregui@mi.unc.edu.ar>
2024-06-14 16:31:30 -0300

    Ej04A-Lab04 formateado

--- Laboratorio_04/ej4/a-array/array.c
+++ Laboratorio_04/ej4/a-array/array.c
@@ -5,16 +5,19 @@
 #define ARRAY_SIZE 7
 
 static void
-print_array(int a[], size_t length) {
+print_array(int a[], size_t length)
+{
     assert(length > 0);
     printf("[ %d", a[0]);
-    for (size_t i=1; i<length; i++) {
+    for (size_t i=1; i<length; i++)
+    {
         printf(", %d", a[i]);
     }
     printf("]\n");
 }
 
-int main(void) {
+int main(void)
+{
     int arr[ARRAY_SIZE] = {7, 4, 1, 3, 9, 3, 5};
     int *p=NULL;
     printf("before: ");
@@ -23,7 +26,8 @@ int main(void) {
     //
     // Se pueden agregar cosas antes del `for` de ser necesario
     //
-    for (unsigned int i=0u; i < ARRAY_SIZE; i++) {
+    for (unsigned int i=0u; i < ARRAY_SIZE; i++)
+    {
         // p = &arr[i];  // FIXME: Se usa operador &
         p = arr + i; // Sin usar operador &
         // *p = 0;       // FIXME: Se usa operador *
--- Laboratorio_04/ej4/b-messi/main.c
+++ Laboratorio_04/ej4/b-messi/main.c
@@ -12,7 +12,12 @@ print_data(data_t d) {
 }
 
 void set_name(name_t new_name, data_t *d) {
-    /* COMPLETAR */
+    unsigned int i = 0;
+    while (new_name[i] != '\0') {
+        d->name[i] = new_name[i];
+        i++;
+    }
+    d->name[i] = '\0';
 }
 
 int main(void) {
@@ -20,7 +25,7 @@ int main(void) {
     data_t messi = {"Leo Messi", 36, 169};
     print_data(messi);
     name_t messi_full_name = "Lionel Messi";
-    /* COMPLETAR */
+    set_name(messi_full_name, &messi);
     print_data(messi);
 
     return EXIT_SUCCESS;
